21624e5c3e908fc640b2f8b5cb984b0dd1289a23
Aliaksandr Kolokolchikov <aliaksandr kolokolchikov>
2022-03-17 19:21:09 +0300

    add

---
+++ script.js
@@ -0,0 +1,91 @@
+const time = document.querySelector("#timer"),
+    greeting = document.querySelector("#greeting"),
+    name = document.querySelector("#name"),
+    enter = document.querySelector("#enter");
+
+const lookAmPa = true;
+
+const lookTime = () => {
+    let today = new Date(),
+        hour = today.getHours(),
+        min = today.getMinutes(),
+        sec = today.getSeconds();
+
+    const amPm = hour >= 12 ? "PM" : "AM";
+
+    hour = hour % 12 || 12;
+
+    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
+        sec
+    )} ${amPm} `;
+
+    setTimeout(lookTime, 1000);
+};
+
+const addZero = (number) => (number < 10 ? "0" + number : number);
+
+const setBg = () => {
+    let today = new Date(),
+        hour = today.getHours();
+
+    if (hour < 12) {
+        greeting.textContent = "Good Morning,";
+        document.body.style.backgroundImage = 'url("./img/morning.jpg")';
+    } else if (hour < 18) {
+        greeting.textContent = "Good Afternoon,";
+        document.body.style.backgroundImage = 'url("./img/afternoon.jpg")';
+    } else {
+        greeting.textContent = "Good Evening,";
+        document.body.style.backgroundImage = 'url("./img/evening.jpg")';
+    }
+};
+
+const setName = (e) => {
+    if (e.type === "keypress") {
+        if (e.key === "Enter") {
+            localStorage.setItem("name", e.target.innerHTML);
+            name.blur();
+        }
+        localStorage.setItem("name", e.target.innerHTML);
+    }
+};
+const getName = () => {
+    if (
+        localStorage.getItem("name") === null ||
+        localStorage.getItem("name").length === 0
+    ) {
+        name.textContent = "[Enter name]";
+    } else {
+        name.textContent = localStorage.getItem("name");
+    }
+};
+
+
+const setFocus = (e) => {
+    if (e.type === "keypress") {
+        if (e.which === 13 || e.keyCode == 13) {
+            localStorage.setItem("focus", e.target.innerText);
+            focus.blur()
+            getFocus();
+        } else {
+            localStorage.setItem("focus", e.target.innerText);
+        }
+    };
+}
+const getFocus = () => {
+    if (
+        localStorage.getItem("focus") === null ||
+        localStorage.getItem("focus").length === 0
+    ) {
+        focus.textContent = "[Enter focus]";
+    } else {
+        focus.textContent = localStorage.getItem("focus")
+    }
+}
+name.addEventListener("keypress", setName);
+name.addEventListener("blur", setName);
+name.addEventListener("keypress", setFocus);
+lookTime();
+setBg();
+getName();
+getFocus();
